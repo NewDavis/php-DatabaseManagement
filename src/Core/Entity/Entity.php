e368d700a77b135fbabad764ce87c9fdbae5c746
NewDavis <davisharder@gmx.de>
2024-08-01 15:58:43 +0200

    - update

--- src/Core/Entity/Entity.php
+++ src/Core/Entity/Entity.php
@@ -41,14 +41,74 @@ class Entity
         return $this->delete;
     }
 
-    public function jsonSerialize(bool $subSerialize = true, int $nestingDepth = 0, int $maxNestingDepth = 10): array
+    public function jsonSerialize(bool $nesting = true, int $nestingDepth = 0, int $maxNestingDepth = 10): array
     {
-        $properties = [];
+        $json = [];
 
-        unset($properties['persisted']);
-        unset($properties['delete']);
+        $reflectionClass = new \ReflectionClass($this);
 
-        return $properties;
+        if($nesting && $nestingDepth < $maxNestingDepth) {
+            $nestingDepth++;
+        }else{
+            $nesting = !$nesting;
+        }
+
+        $json['entity'] = $this->convertEntityClassName($this);
+        $json['entity_name'] = $this->getDefinitionClass()::ENTITY_NAME;
+
+        foreach ($reflectionClass->getProperties() as $property) {
+            $value = null;
+
+            if($property->isInitialized($this)) {
+                $value = $property->getValue($this);
+            }
+
+            if($value instanceof Entity) {
+                if($nesting) {
+                    if($value != null) {
+                        //relation
+                        $value = $value->jsonSerialize($nesting, $nestingDepth, $maxNestingDepth);
+                    }
+
+                    $json[$property->getName()] = $value;
+                }
+
+                continue;
+            } else if($value instanceof EntityCollection) {
+                if($nesting) {
+                    //relation (collection)
+                    $json[$property->getName()] = [];
+                    foreach ($value as $entity) {
+                        $serialized = $entity->jsonSerialize($nesting, $nestingDepth, $maxNestingDepth);
+
+                        $json[$property->getName()][] = $serialized;
+                    }
+                }
+
+                continue;
+            }
+
+            $json[$property->getName()] = $value;
+        }
+
+        unset($json['persisted']);
+        unset($json['delete']);
+
+        return $json;
+    }
+
+    public static function convertEntityClassName(string|object $class, bool $replaceWithDashes = true) : string
+    {
+        if($replaceWithDashes) {
+            if (is_object($class)) {
+                $entityClass = get_class($class);
+            } else {
+                $entityClass = $class;
+            }
+            $entityClass = str_replace("\\", '-', $entityClass);
+        }
+
+        return $entityClass;
     }
 
 }
