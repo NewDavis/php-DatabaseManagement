9a03fa4e6b88fb43d9fb592e839849a804858ef6
DavisHarder <davis.harder@econsor.de>
2025-06-13 15:18:17 +0200

    fixed infinite loop

--- src/Core/Entity/Entity.php
+++ src/Core/Entity/Entity.php
@@ -20,31 +20,34 @@ abstract class Entity implements EntityInterface
     use CreatedAtTrait;
     use UpdatedAtTrait;
 
-    public function jsonSerialize(): array
+    public function jsonSerialize(array &$visited = []): array
     {
+        $objectId = spl_object_id($this);
+        if (isset($visited[$objectId])) {
+            return ['_ref' => $objectId]; // or null / ID / whatever makes sense
+        }
+        $visited[$objectId] = true;
+
         $json = [];
 
         $reflectionClass = new \ReflectionClass($this);
-
         $json['entity'] = $this->getDefinitionClass()::getEntityName();
 
         foreach ($reflectionClass->getProperties() as $property) {
             $value = null;
 
-            if($property->isInitialized($this)) {
+            if ($property->isInitialized($this)) {
                 $value = $property->getValue($this);
             }
 
             if ($value instanceof Entity) {
-                $json[$property->getName()] = $value->jsonSerialize();
-
+                $json[$property->getName()] = $value->jsonSerialize($visited);
                 continue;
-            } else if ($value instanceof EntityCollection) {
+            } elseif ($value instanceof EntityCollection) {
                 $json[$property->getName()] = [];
                 foreach ($value->getEntities() as $entity) {
-                    $json[$property->getName()][] = $entity->jsonSerialize();
+                    $json[$property->getName()][] = $entity->jsonSerialize($visited);
                 }
-
                 continue;
             }
 
