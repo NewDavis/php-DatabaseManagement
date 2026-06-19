eeb563ad0a0c55a928003fe94e365c35cf692daf
DavisHarder <davis.harder@econsor.de>
2026-06-19 14:21:09 +0200

    - fixed JSONFieldSerializer

--- src/Entity/FieldSerializer/JSONFieldSerializer.php
+++ src/Entity/FieldSerializer/JSONFieldSerializer.php
@@ -6,11 +6,19 @@ class JSONFieldSerializer extends AbstractFieldSerializer
 {
     public function encode(mixed $value): string
     {
+        if (is_string($value)) {
+            return $value;
+        }
+
         return json_encode($value);
     }
 
     public function decode(mixed $data): mixed
     {
+        if (is_array($data)) {
+            return $data;
+        }
+
         return json_decode($data, true);
     }
 
